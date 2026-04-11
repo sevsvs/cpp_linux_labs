6cb9691cb17dbf759bdd7a6f867c58a8c4943eae
sevsvs <dr.pogorelko4@gmail.com>
2026-04-11 10:50:59 +0300

    listing

--- lab_02/src/counter.cpp
+++ lab_02/src/counter.cpp
@@ -6,43 +6,41 @@ using namespace std;
 
 float iteratedFunction(float x) {
 
-return x*x -x*x + 4*x - 5*x + x + x;
+	return x*x -x*x + 4*x - 5*x + x + x;
 
 }
 
 int main() {
-char repeat;
+	char repeat;
 
-do {
-int n;
-cout << "Type a number of iterations: ";
-cin >> n;
+	do {
+		int n;
+		cout << "Type a number of iterations: ";
+		cin >> n;
 
-cout << endl;
+		cout << endl;
 
-if (cin.fail()) {
+		if (cin.fail()) {
+			cout << "Invalid value" << endl;
+			return 1;
+		}
 
-cout << "Invalid value" << endl;
-return 1;
-}
+		float x = 0.13;
+		float accum = 0.0;
+		auto start = chrono::high_resolution_clock::now();
 
-float x = 0.13;
-float accum = 0.0;
-auto start = chrono::high_resolution_clock::now();
+		for (int i = 0; i < n; i++) {
+			accum += iteratedFunction(x);
+		}
 
-for (int i =0; i< n; i++) {
-accum += iteratedFunction(x);
-}
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> delta = end - start;
+		cout << "Time for " << n << " iterations: " << delta.count() << " seconds" << endl;
 
-auto end = chrono::high_resolution_clock::now();
-chrono::duration<double> delta = end - start;
-cout << "Time for " << n << " iterations: " << delta.count() << " seconds" << endl;
+		cerr << "Accumulated value " << accum << endl;
 
-cerr << "Accumulated value " << accum << endl;
-
-cout << "repeat?";
-cin >> repeat;
-} while (repeat == 'y');
-return 0;
-
+		cout << "repeat?";
+		cin >> repeat;
+		} while (repeat == 'y');
+	return 0;
 }
